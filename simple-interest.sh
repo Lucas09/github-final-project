04dfe4b7c6ccb1b12f90efa8273aa634bbaf8952
Lucas09 <simper.lucas@gmail.com>
2022-11-01 21:22:42 +0100

    Update simple-interest.sh
    Task 7

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Lucas
    # Additional Authors:
-   # <your GitHub username>
+   # Lucas09
 
    # Input:
    # p, principal amount
